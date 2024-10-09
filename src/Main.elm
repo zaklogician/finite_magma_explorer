7c9ed169fffd68fa1f9b1645a2dd3fed60d0204f
Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>
2024-10-09 13:22:40 +0000

    Prepare for move to project repo 3
    Signed-off-by: Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>

--- src/Main.elm
+++ src/Main.elm
@@ -279,7 +279,7 @@ view model =
       , viewRightPanel model
       ]
     , div [class "copyright"]
-      [text ("© 2024 The Equational Theories Project / data: " ++ model.version)]
+      [text ("© 2024 The Equational Theories Project / data: " ++ take 6 model.version)]
     ]
 
 
